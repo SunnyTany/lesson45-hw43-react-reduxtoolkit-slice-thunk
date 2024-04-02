41faa299ae3d2a302c02e7ae0c68bc1a2cd3ca3d
sunnytanyusha <sunnytanyusha@gmail.com>
2024-04-02 11:02:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + TypeScript + Vite + redux + slice + thunk
+
+- Слайс counterSlice створений за допомогою функції createSlice від Redux Toolkit.
+- Існуючі редюсер та екшени - сгенеровані слайсом.
+- Підключений слайс до стору за допомогою функції configureStore.
---
+++ src/redux/counterSlice.ts
@@ -0,0 +1,41 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { AppThunk } from './store';
+
+export type CounterState = {
+  count: number
+};
+
+const initialState: CounterState = {
+  count: 0,
+}
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    incrementSuccess: (state, action: PayloadAction<number>) => {
+      state.count += action.payload
+    },
+    decrementSuccess: (state, action: PayloadAction<number>) => {
+      state.count -= action.payload
+    },
+  },
+})
+
+
+
+export const incrementAsync = (): AppThunk => async (dispatch) => {
+  setTimeout(() => {
+    dispatch(incrementSuccess(1))
+  }, 1000)
+}
+
+export const decrementAsync = (): AppThunk => async (dispatch) => {
+  setTimeout(() => {
+    dispatch(decrementSuccess(1))
+  }, 1000)
+}
+
+export const { incrementSuccess, decrementSuccess } = counterSlice.actions
+
+export default counterSlice.reducer
